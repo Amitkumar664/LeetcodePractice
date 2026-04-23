0df83391150a4382d8c7b324fd187e92b1c913b9
Amit Kumar <amitkumar4102004b@gmail.com>
2026-04-23 09:09:46 +0530

    [Day 2] added day2 leetcode question

---
+++ RemoveElement.java
@@ -0,0 +1,28 @@
+public class RemoveElement {
+
+    public static int removeElement(int[] nums, int val) {
+        int c = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[c] = nums[i];
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int k = removeElement(nums, val);
+
+        System.out.println("New length: " + k);
+        System.out.print("Updated array: ");
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
